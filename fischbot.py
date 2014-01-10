a6c58425428f61b0cc2dbb33ad0a2c777216b315
Gubolin <gubolin@fantasymail.de>
2014-01-10 19:54:06 +0100

    evil commit

--- fischbot.py
+++ fischbot.py
@@ -147,7 +147,7 @@ while True:
             send2chan('Test received.')
 
         if atbegin('!blame', data):
-            send2chan('It\'s Casimo\'s fault!')
+            send2chan('It\'s flyingfisch\'s fault!')
 
         if atbegin('!authfischbot', data):
             irc.send('PRIVMSG X3 :auth fischbot ' + data.split(' ')[4] + '\r\n')
